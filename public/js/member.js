367bf3c892217e2c430ff1d16b4fb5eac1c92257
nancy <nancy65018@gmail.com>
2022-05-07 20:14:31 +0800

    [fix]:fix template to improve user experience

--- public/js/member.js
+++ public/js/member.js
@@ -1,6 +1,6 @@
+//// edit user register info
 const nameContainer = document.querySelector('.name-img-container');
 const pwdContainer = document.querySelector('.pwd-img-container');
-
 const editUserInfoController = (e) => {
     const actiontype = e.target.className.slice(0,4);
     const target = e.target.className.slice(5);
@@ -23,3 +23,21 @@ const editUserInfoController = (e) => {
 }
 nameContainer.addEventListener('click', editUserInfoController);
 pwdContainer.addEventListener('click', editUserInfoController);
+
+//// click edit btn to show profile form
+const editProfileBtn = document.querySelector('.edit-profile');
+const profileContainer = document.querySelector('.profile-container');
+const showProfile = () => {
+    editProfileBtn.classList.add('hide');
+    profileContainer.classList.remove('hide');
+}
+editProfileBtn.addEventListener('click', showProfile);
+
+//// profile form controller
+const profileForm = document.querySelector('form');
+const saveProfileController = (e) => {
+    e.preventDefault();
+    editProfileBtn.classList.remove('hide');
+    profileContainer.classList.add('hide');
+}
+profileForm.addEventListener('submit', saveProfileController);
