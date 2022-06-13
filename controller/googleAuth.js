e33171dae61fc873d807bc76cb7d605c382fc067
nancy <nancy65018@gmail.com>
2022-06-13 16:15:05 +0800

    [fix]:rename file

--- controller/googleAuth.js
+++ controller/googleAuth.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.CALLBACK_URL
+  callbackURL: "/api/user/google/callback"
   //change to process.env.CALLBACK_URL
 },
   (accessToken, refreshToken, profile, cb) => {
