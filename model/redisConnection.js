bd3fbfcd1e08bd5d00f35cf8b297ab1aebfdbb71
nancy <nancy65018@gmail.com>
2022-06-13 16:16:23 +0800

    [fix]:remove useless code

--- model/redisConnection.js
+++ model/redisConnection.js
@@ -2,7 +2,7 @@ const redis = require('redis');
 require('dotenv').config();
 const url = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
 const client = redis.createClient({
-    // url
+    url
 });
 client.connect();
 
